0e26b7ec9ac595c5a734c61d260d679dd77d0503
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-02-09 15:30:00 +0600

    1

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,30 @@
-console.log("hlw");
+const clickHere = () => {
+    const search = document.getElementById("search").value
+    
+    fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+    
+        .then((response) => response.json())
+        .then((data) => foodCategories(data.categories))
+
+    const foodCategories = category => {
+        const div = document.getElementById("result")
+        category.forEach((food) => {
+            const newFood = food.strCategory;
+
+            const foodDiv = document.createElement("div")
+            foodDiv.className = "foods"
+            const foodInfo = `
+                          
+                          <img src="${food.strCategoryThumb}"></img> 
+                          <h1>${newFood}</h1>     
+             `
+            foodDiv.innerHTML = foodInfo
+            div.appendChild(foodDiv)
+
+
+
+        })
+
+    }
+
+}
